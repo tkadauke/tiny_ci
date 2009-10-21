0993efdcdbe6e17ab80fd6e834695ddc79920d0c
Thomas Kadauke <tkadauke@imedo.de>
2009-10-22 01:14:57 +0200

    capture build output

--- app/models/build.rb
+++ app/models/build.rb
@@ -2,15 +2,34 @@ class Build < ActiveRecord::Base
   belongs_to :project
   acts_as_list :scope => :project_id
   
-  after_create :enqueue
+  named_scope :pending, :conditions => { :status => 'pending' }
   
   def build!
+    update_attributes :status => 'running'
+    
+    capture_output %{script/runner "Build.find(#{id}).build_without_background!"}
+  end
+  
+  def build_without_background!
     SimpleCI::DSL.evaluate(project.name, project.steps)
     update_attributes :status => 'success'
   end
-  background_method :build!
   
-  def enqueue
-    SimpleCI::Queue.enqueue(project)
+  def buildable?
+    project.buildable?
+  end
+  
+private
+  def capture_output(command)
+    IO.popen("#{command} 2>&1") do |stdout|
+      output = ""
+      while !stdout.eof?
+        if line = stdout.gets
+          puts output
+          output << line
+          reload.update_attributes(:output => output)
+        end
+      end
+    end
   end
 end
--- lib/simple_ci/queue_runner.rb
+++ lib/simple_ci/queue_runner.rb
@@ -1,30 +1,12 @@
-require 'drb'
-
 module SimpleCI
   class QueueRunner
     def self.run
-      @queue = []
-      
-      # start up the DRb service
-      DRb.start_service "druby://localhost:2250", self
-      
       loop do
-        sleep 1
-        next if @queue.empty?
-        
-        project = ::Project.find(@queue.first)
-        if project.buildable?
-          project.build!
-          @queue.shift
-        end
+        builds = ::Build.pending.find(:all)
+        next_build = builds.find { |build| build.buildable? }
+        next_build.build! if next_build
+        sleep 2
       end
-
-      # wait for the DRb service to finish before exiting
-      DRb.thread.join
-    end
-    
-    def self.enqueue(project)
-      @queue << project
     end
   end
 end
