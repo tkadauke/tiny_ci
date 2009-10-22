2d34e1ff846583d32454f4bb59c1d77c592c6bee
Thomas Kadauke <tkadauke@imedo.de>
2009-10-22 09:18:56 +0200

    rm background plugin

--- vendor/plugins/background_lite/lib/core_ext/error_reporters/stderr_error_reporter.rb
+++
@@ -1,9 +0,0 @@
-module BackgroundLite
-  # Reports error on $stdout.
-  class StderrErrorReporter
-    # Prints the exception's error message on $stderr.
-    def self.report(error)
-      $stderr.puts error.message
-    end
-  end
-end
--- vendor/plugins/background_lite/lib/core_ext/object.rb
+++
@@ -1,7 +0,0 @@
-class Object
-  # Clones the object to for transmission to the background process. The default
-  # implementation is dupping the object.
-  def clone_for_background
-    dup
-  end
-end
