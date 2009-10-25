6fd69cfa425a51d314db5c836311fee107ac135d
Thomas Kadauke <tkadauke@imedo.de>
2009-10-25 21:05:17 +0100

    refactor report rendering to use actionview

--- app/helpers/builds_helper.rb
+++ app/helpers/builds_helper.rb
@@ -1,2 +1,10 @@
 module BuildsHelper
+  def render_report(report, version)
+    template_path = "#{RAILS_ROOT}/lib/simple_ci/report/templates/#{version}/#{report.class.name.underscore.split('/').last}.html.erb"
+    begin
+      render :file => template_path, :locals => { :report => report }
+    rescue ActionView::MissingTemplate
+      ""
+    end
+  end
 end
--- lib/simple_ci/report/base.rb
+++ lib/simple_ci/report/base.rb
@@ -2,16 +2,6 @@ module SimpleCI
   module Report
     class Base
       attr_accessor :raw_output
-      
-      def to_html(version)
-        template_path = File.dirname(__FILE__) + "/templates/#{version}/#{self.class.name.underscore.split('/').last}.html.erb"
-        if File.exist?(template_path)
-          report = self
-          ERB.new(File.read(template_path)).result(binding)
-        else
-          ""
-        end
-      end
     end
   end
 end
