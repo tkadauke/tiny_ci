4df67b2850e953d5303c8021a280099a12011bcd
Thomas Kadauke <tkadauke@imedo.de>
2009-10-29 10:36:51 +0100

    add ssh gem to environment

--- config/environment.rb
+++ config/environment.rb
@@ -17,6 +17,7 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem 'fastercsv'
   config.gem 'juggernaut'
+  config.gem 'net-ssh', :lib => "net/ssh"
   
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
