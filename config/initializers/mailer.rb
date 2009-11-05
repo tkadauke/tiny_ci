139c81dcf177d24d44818d5307a727e576f83d1d
Thomas Kadauke <tkadauke@imedo.de>
2009-11-05 20:50:02 +0100

    fix embarrasing mistake :-)

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-  :domain => SimpleCI::Config.email_domain,
-  :address => SimpleCI::Config.email_address,
-  :port => SimpleCI::Config.email_port,
-  :user_name => SimpleCI::Config.email_user_name,
-  :password => SimpleCI::Config.email_password,
-  :authentication => SimpleCI::Config.email_authentication
+  :domain => TinyCI::Config.email_domain,
+  :address => TinyCI::Config.email_address,
+  :port => TinyCI::Config.email_port,
+  :user_name => TinyCI::Config.email_user_name,
+  :password => TinyCI::Config.email_password,
+  :authentication => TinyCI::Config.email_authentication
 }
