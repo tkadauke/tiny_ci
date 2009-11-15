1d18f460c4251a4a1a1f40ae5c2d6348238b763c
Thomas Kadauke <tkadauke@imedo.de>
2009-11-15 03:00:26 +0100

    version constant

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,7 @@ RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+require File.join(File.dirname(__FILE__), 'version')
 
 if ENV['SETUP'] == 'true'
   class Rails::Initializer
