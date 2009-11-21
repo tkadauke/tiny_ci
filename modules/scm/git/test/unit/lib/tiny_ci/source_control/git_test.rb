6acb602efdb9d0f466e607f3c68d123bc7f08a25
Thomas Kadauke <tkadauke@imedo.de>
2009-11-21 17:57:33 +0100

    fixes

--- modules/scm/git/test/unit/lib/tiny_ci/source_control/git_test.rb
+++ modules/scm/git/test/unit/lib/tiny_ci/source_control/git_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../../../test_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../../../test_helper')
 
 class TinyCI::SourceControl::GitTest < ActiveSupport::TestCase
   test "should do the update workflow" do
